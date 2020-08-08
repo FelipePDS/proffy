85a794b25cc75facab9efa5918d93e69877302d4
FelipePDS <felipepdasilva66@gmail.com>
2020-08-08 04:43:15 -0300

    NLW version

---
+++ src/server.js
@@ -0,0 +1,27 @@
+//SERVIDOR
+const express = require('express')
+const server = express()
+
+//PAGES
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
+
+//CONFIGURAR O NUNJUCKS (template engine)
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true, //não guardar as alterações
+})
+
+//INICIO E CONFIGURAÇÃO DO SERVIDOR
+server
+//receber os dados do req.body
+.use(express.urlencoded({ extended: true }))
+//CONFIGURAR ARQUIVOS ESTÁTICOS (CSS, SCRIPTS, IMAGES)
+.use(express.static("public"))
+// ROTAS DE APLICAÇÃO
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
+//START DO SERVIDOR
+.listen(5500)
